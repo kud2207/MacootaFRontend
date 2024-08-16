aa9784aa49d69953a8ceb4fd37ef9d4c4f57ebb6
kud2207 <ulrichkageu@gmail.com>
2024-08-16 18:06:20 +0200

    build projet

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Home from "../components/MainPage/Home"
-import FlashDeals from "../components/flashDeals/FlashDeals"
 import TopCate from "../components/top/TopCate"
 import NewArrivals from "../components/newarrivals/NewArrivals"
 import Discount from "../components/discount/Discount"
